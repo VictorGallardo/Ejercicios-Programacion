8d274248d3a6c8e320502bca6269eaf523306d20
Víctor Gallardo Luque <vbubobuho@gmail.com>
2019-10-10 17:40:14 +0200

    Ejercicios programación.

---
+++ Tema 5/Ejercicio06.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * 
+ * 
+ */
+public class Ejercicio06 {
+	
+	public static void main (String[] args) {
+		 
+    System.out.println("Este programa le mostrará los números del 320 al 160, contando de 20 en 20 hacia atrás.");
+    
+    int n = 320;
+    
+    do {
+      System.out.println(n);
+      n -= 20;
+    } while (n >= 160);
+	}
+}
+
