bed6e89be470f833ee5878d125a4bacc2037e45e
Víctor Gallardo Luque <vbubobuho@gmail.com>
2019-10-19 14:14:58 +0200

    Ejercicios progrmación.

---
+++ Tema 5/Ejercicio21.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * 
+ */
+public class Ejercicio21 {
+  
+  public static void main (String[] args) {
+    
+    System.out.println("Por favor, introduzca numeros pulsando enter cuando introduzca uno negativo");
+    System.out.print("el programa parará.");
+
+
+
+
+    
+    
+  }
+}
+
