3d8e6ff505c445c718932a650b9aa5ec9e84e183
Víctor Gallardo <victor.gallardo@iescampanillas.com>
2019-10-20 20:43:35 +0200

    Ejercicios progrmación

--- Tema 5/Ejercicio21.java
+++ Tema 5/Ejercicio21.java
@@ -7,7 +7,7 @@ public class Ejercicio21 {
   public static void main (String[] args) {
     
     System.out.println("Por favor, introduzca numeros pulsando enter cuando introduzca uno negativo");
-    System.out.print("el programa parará.");
+    System.out.println("el programa parará.");
 
 
 
