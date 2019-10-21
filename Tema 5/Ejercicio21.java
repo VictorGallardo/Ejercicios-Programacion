24461f3996e2b038f556c39da5cf51bf32b982a8
Víctor Gallardo <victor.gallardo@iescampanillas.com>
2019-10-21 11:10:24 +0200

    Ejercicios programación.

--- Tema 5/Ejercicio21.java
+++ Tema 5/Ejercicio21.java
@@ -1,19 +1,37 @@
-/**
- *
- * 
- */
+import java.util.Scanner;
+
 public class Ejercicio21 {
   
   public static void main (String[] args) {
+    Scanner s = new Scanner(System.in);
     
-    System.out.println("Por favor, introduzca numeros pulsando enter cuando introduzca uno negativo");
-    System.out.println("el programa parará.");
-
-
-
-
+    System.out.println("Por favor, introduzca numeros pulsando enter cuando introduzca uno negativo el programa parará.");
     
+    int numeroIntroducido = 1; //he puesto uno para que entre en el while.
+    int contador = 0;
+    int suma = 0;
+    int numeroMayor = numeroIntroducido;
+    int contadorImpar = 0;
     
+    // Nos cuenta los números introducidos hasta que se introduce uno negativo.
+    while (numeroIntroducido > 0) {
+      numeroIntroducido = Integer.parseInt(s.nextLine());
+      contador++;
+      // Número mas alto de los pares.
+      if (numeroIntroducido % 2 == 0) {
+        if (numeroIntroducido > numeroMayor) {
+          numeroMayor = numeroIntroducido;
+        }
+      } else { 
+        suma += numeroIntroducido;
+        contadorImpar++;
+    }
+    }
+    //Es la cuenta de los números introducidos.
+    System.out.println("Ha introducido " + (contador - 1) + " números.");
+    System.out.println("De los numeros pares introducidos " + numeroMayor + " es el mayor.");
+    System.out.println("La media de los números impares es " + suma / contadorImpar);
+
   }
 }
 
---
+++ Tema 5/Ejercicio69.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Ejercicio69 {
+  
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    System.out.print("Introduzca la altura de la pirámide: ");
+    int altura = Integer.parseInt(s.nextLine());
+    
+    int caracteresPorFila = 1;
+    int espaciosDelanteros = altura;
+    
+    for (int i = 0; i < altura; i++) {
+      // Pinta los espacios delanteros en cada fila
+      for (int j = 0; j < espaciosDelanteros; j++) {
+        System.out.print(" ");
+      }
+      
+      // Pinta el flanco izquierdo
+      for (int j = 0; j < caracteresPorFila; j++) {
+        System.out.print("*");
+      }
+      
+      // Pinta la parte central
+      if (i % 2 == 0) { // cuando i es par
+        System.out.print("****");
+      } else {
+        System.out.print("    ");
+      }
+      
+      // Pinta el flanco derecho
+      for (int j = 0; j < caracteresPorFila; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+      
+      caracteresPorFila++;
+      espaciosDelanteros--;
+    }
+    
+  }
+  
+}
+
