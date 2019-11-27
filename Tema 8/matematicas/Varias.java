7d772fe4c592fac439dded352981b93fe52470ac
Víctor Gallardo <victor.gallardo@iescampanillas.com>
2019-11-27 10:09:40 +0100

    Ejercicios programación

--- Tema 8/matematicas/Varias.java
+++ Tema 8/matematicas/Varias.java
@@ -231,13 +231,7 @@ public class Varias {
 
   
   /**
-   * Devuelve el dígito que está en la posición <code>n</code> de un número
-   * entero. Se empieza contando por el 0 y de izquierda a derecha.
    *
-   * @param x número entero
-   * @param n posición dentro del número <code>x</code>
-   * @return  dígito que está en la posición n del número <code>x</code>
-   *          empezando a contar por el 0 y de izquierda a derecha
    */
   public static int digitoN(long x, int n) {
     x = voltea(x);
@@ -250,13 +244,7 @@ public class Varias {
   }
   
   /**
-   * Devuelve el dígito que está en la posición n de un número entero. Se
-   * empieza contando por el 0 y de izquierda a derecha.
    *
-   * @param x número entero
-   * @param n posición dentro del número <code>x</code>
-   * @return  dígito que está en la posición n del número <code>x</code>
-   *          empezando a contar por el 0 y de izquierda a derecha
    */
   public static int digitoN(int x, int n) {
     return digitoN((long)x, n);
@@ -267,13 +255,7 @@ public class Varias {
   
   
   /**
-   * Da la posición de la primera ocurrencia de un dígito dentro de un número
-   * entero. Si no se encuentra, devuelve -1.
-   *
-   * @param x número entero
-   * @param d dígito a buscar dentro del número
-   * @return  posición de la primera ocurrencia del dígito dentro del número o
-   *          -1 si no se encuentra
+   * 
    */
   public static int posicionDeDigito(long x, int d) {
     int i;
@@ -287,14 +269,8 @@ public class Varias {
     }
   }
   
-    /**
-   * Da la posición de la primera ocurrencia de un dígito dentro de un número
-   * entero. Si no se encuentra, devuelve -1.
+  /**
    *
-   * @param x número entero
-   * @param d dígito a buscar dentro del número
-   * @return  posición de la primera ocurrencia del dígito dentro del número o
-   *          -1 si no se encuentra
    */
   public static int posicionDeDigito(int x, int d) {
     return posicionDeDigito((long)x, d);
@@ -305,24 +281,14 @@ public class Varias {
   
   
   /**
-   * Le quita a un número <code>n</code> dígitos por detrás (por la derecha). 
    *
-   * @param x número entero
-   * @param n número de dígitos que se le van a quitar
-   * @return  número inicial <code>x</code> con <code>n</code> dígitos menos
-   *          quitados de la derecha
    */
   public static long quitaPorDetras(long x, int n) {
     return x / (long)potencia(10, n);
   }
   
-    /**
-   * Le quita a un número <code>n</code> dígitos por detrás (por la derecha). 
+  /**
    *
-   * @param x número entero
-   * @param n número de dígitos que se le van a quitar
-   * @return  número inicial <code>x</code> con <code>n</code> dígitos menos
-   *          quitados de la derecha
    */
   public static int quitaPorDetras(int x, int n) {
 
@@ -334,11 +300,7 @@ public class Varias {
   
   
   /**
-   * Le quita a un número <code>n</code> dígitos por delante (por la izquierda).                                                 *
-   * @param x número entero
-   * @param n número de dígitos que se le van a quitar
-   * @return  número inicial <code>x</code> con <code>n</code> dígitos menos
-   *          quitados de la izquierda
+   *
    */
   public static long quitaPorDelante(long x, int n) {
     x = pegaPorDetras(x, 1); // "cierra" el número por si acaso termina en 0
@@ -348,11 +310,7 @@ public class Varias {
   }
 
   /**
-   * Le quita a un número <code>n</code> dígitos por delante (por la izquierda).                                                 *
-   * @param x número entero
-   * @param n número de dígitos que se le van a quitar
-   * @return  número inicial <code>x</code> con <code>n</code> dígitos menos
-   *          quitados de la izquierda
+   *
    */
   public static int quitaPorDelante(int x, int n) {
     return (int)quitaPorDelante((long)x, n);
@@ -362,24 +320,14 @@ public class Varias {
   
   
   /**
-   * Añade un dígito a un número por detrás (por la derecha).
    *
-   * @param x número entero
-   * @param d dígito que se le va a pegar por la derecha
-   * @return  número inicial <code>x</code> con el dígito <code>d</code> pegado
-   *          por la derecha
    */
   public static long pegaPorDetras(long x, int d) {
     return juntaNumeros(x, d);
   }
 
   /**
-   * Añade un dígito a un número por detrás (por la derecha).
    *
-   * @param x número entero
-   * @param d dígito que se le va a pegar por la derecha
-   * @return  número inicial <code>x</code> con el dígito <code>d</code> pegado
-   *          por la derecha
    */
   public static int pegaPorDetras(int x, int d) {
     return (int)pegaPorDetras((long)x, d);
@@ -388,24 +336,14 @@ public class Varias {
   //----------------------------------------------------------------------------
 
   /**
-   * Añade un dígito a un número por delante (por la izquierda).
-   *
-   * @param x número entero
-   * @param d dígito que se le va a pegar por la izquierda
-   * @return  número inicial <code>x</code> con el dígito <code>d</code> pegado
-   *          por la izquierda
+   * 
    */
   public static long pegaPorDelante(long x, int d) {
     return juntaNumeros(d, x);
   }
 
   /**
-   * Añade un dígito a un número por delante (por la izquierda).
-   *
-   * @param x número entero
-   * @param d dígito que se le va a pegar por la izquierda
-   * @return  número inicial <code>x</code> con el dígito <code>d</code> pegado
-   *          por la izquierda
+   * 
    */
   public static int pegaPorDelante(int x, int d) {
     return (int)pegaPorDelante((long)x, d);
@@ -416,16 +354,7 @@ public class Varias {
 
   
   /**
-   * Toma como parámetros las posiciones inicial y final dentro de un número y
-   * devuelve el trozo correspondiente.
-   * <p>
-   * Las posiciones se cuentan de izquierda a derecha comenzando por el cero.
-   *
-   * @param x      número entero
-   * @param inicio posición inicial
-   * @param fin    posición final
-   * @return       trozo de número compuesto por todos los dígitos que van desde
-   *               la posición inicial a la posición final incluyendo ambos
+   * 
    */
   public static long trozoDeNumero(long x, int inicio, int fin) {
     int longitud = digitos(x);
@@ -435,16 +364,7 @@ public class Varias {
   }
 
   /**
-   * Toma como parámetros las posiciones inicial y final dentro de un número y
-   * devuelve el trozo correspondiente.
-   * <p>
-   * Las posiciones se cuentan de izquierda a derecha comenzando por el cero.
-   *
-   * @param x      número entero
-   * @param inicio posición inicial
-   * @param fin    posición final
-   * @return       trozo de número compuesto por todos los dígitos que van desde
-   *               la posición inicial a la posición final incluyendo ambos
+   * 
    */
   public static int trozoDeNumero(int x, int inicio, int fin) {
     return (int)trozoDeNumero((long)x, inicio, fin);
@@ -454,22 +374,14 @@ public class Varias {
 
   
   /**
-   * Pega dos números para formar uno solo.
-   *
-   * @param x trozo que se pegará por la izquierda
-   * @param y trozo que se pegará por la derecha
-   * @return  número compuesto de los trozos <code>x</code> e <code>y</code>
+   * 
    */
   public static long juntaNumeros(long x, long y) {
     return (long)(x * potencia(10, digitos(y))) + y;
   }
 
   /**
-   * Pega dos números para formar uno solo.
-   *
-   * @param x trozo que se pegará por la izquierda
-   * @param y trozo que se pegará por la derecha
-   * @return  número compuesto de los trozos <code>x</code> e <code>y</code>
+   * 
    */
   public static int juntaNumeros(int x, int y) {
     return (int)(juntaNumeros((long)x, (long)y));
